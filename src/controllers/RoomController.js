cf173048b65e0d6f650b5c4a4b036fd1628dba26
jeanAt2 <jean0king@gmail.com>
2021-08-09 22:18:19 -0300

    back and parte 1

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,20 @@
+const Database = require("../DB/config");
+module.exports = {
+  async create(req, res) {
+    const db = await Database();
+    const pass = req.body.password;
+    let roomId;
+
+    for (let i = 0; i < 6; i++) {
+      i == 0
+        ? (roomId = Math.floor(Math.random() * 10).toString())
+        : (roomId += Math.floor(Math.random() * 10).toString());
+    }
+
+    await db.run(
+      `INSERT INTO rooms (roomId,passwd) VALUES (${parseInt(roomId)}, ${pass})`
+    );
+    await db.close();
+    res.redirect(`/room/${roomId}`);
+  },
+};
